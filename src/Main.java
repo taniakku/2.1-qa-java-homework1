cc828699948720522c14e8a9f7ead4c1b3c51645
taniakko <tatiennekzm@gmail.com>
2021-11-29 20:24:55 +0300

    Miles

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 1200;
+        int bonusMiles = (ticketPrice / 20);
+        System.out.println("Вам начислено " + bonusMiles + " бонусных миль.");
+
+    }
+}
